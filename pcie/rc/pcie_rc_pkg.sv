a6810f496b7c920597ec79d942328217e2a0a134
maheshvainsh <maheshvainsh552gmail.com>
2025-08-29 23:41:43 +0530

    removed separet agent and added common agent by separated agents

--- pcie/rc/pcie_rc_pkg.sv
+++
@@ -1,8 +0,0 @@
-package pcie_rc_pkg;
-import uvm_pkg::*;//compile uvm lib
-`include "uvm_macros.svh"
-`include "pcie_rc_monitor.sv"
-`include "pcie_rc_driver.sv"
-`include "pcie_rc_sequencer.sv"
-`include "pcie_rc_agent.sv"
-endpackage : pcie_rc_pkg
