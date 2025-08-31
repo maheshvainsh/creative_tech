eb89b5e3e91e96e5ceb7d98185747de2427e4b3b
maheshvainsh <maheshvainsh552gmail.com>
2025-08-31 14:03:11 +0530

    added dummy class for mem model

---
+++ pcie/mem_model/mem_model.sv
@@ -0,0 +1,22 @@
+//classs mem model
+//add[]
+//data
+
+
+
+
+
+//write(add, data)
+
+  //writmemh("txt", add,data,byte enable)
+//add++
+
+
+
+
+
+//read(ref data)
+//readmemh(data)
+
+
+//endclass
